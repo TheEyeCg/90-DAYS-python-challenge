7eb62914ce21653ecec4ffaf722545909aa4b856
root <root@kelvinlina.theeye>
2025-01-09 14:19:13 +0100

    simple basic arithmetic

---
+++ DAY1.py
@@ -0,0 +1,19 @@
+#!/bin/usr/env python3
+#greetings
+print ("Hello World")
+
+#basic arithemetic calculation
+
+a=5
+b=8
+#addition
+print (f"{a} + {b} ={a+b}")
+
+#subtraction
+print (f"{a} - {b} ={a-b}")
+
+#multiplipication
+print (f"{a} * {b} ={a*b}")
+
+#division
+print (f"{a} /{b} ={a/b}")
