3bc80365ba230e3b377765173c47e6d30239ed8c
root <root@kelvinlina.theeye>
2025-01-15 22:22:04 +0100

    libraries

---
+++ DAY10.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import math
+
+# Input from the user
+num = float(input("Enter a number: "))
+
+# Calculating the square root
+sqrt = math.sqrt(num)
+
+# Displaying the result
+print(f"The square root of {num} is {sqrt:.2f}")
