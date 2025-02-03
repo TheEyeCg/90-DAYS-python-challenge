5ba9187ccd587e6158a3c044d6b2799e321594ce
root <root@kelvinlina.theeye>
2025-02-03 23:25:39 +0100

    tweaking variables command

---
+++ variable_practice.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+name = ' kelvinlina '
+print(f"hi{name} would you like to learn python today")
+print(name.title())
+print(name.upper())
+print(name.lower())
+famous_quote = "failure is the root of success"
+famous_person = 'lina'
+message = famous_quote
+print(f"{famous_person} once said {message}")
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+print('\truth\nlina\n\tcaleb')
+file_name = 'python_crash_course.txt'
+print(file_name.removeprefix('python_crash_'))
+print(file_name.removesuffix('.txt'))
